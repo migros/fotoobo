dcb5ceaaf08d9904cc814c4e5a257c7b1eb4dad2
Patrik Spiess <patrik.spiess@mgb.ch>
2023-03-31 11:00:10 +0200

    🏷️ update typing (was ignored before)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,12 @@
 """The pytest global fixtures"""
 
+from pathlib import Path
+
+import _pytest
 import pytest
 
 
 @pytest.fixture(scope="session")
-def temp_dir(tmp_path_factory):  # type: ignore # (it's a pathlib.Path object)
+def temp_dir(tmp_path_factory: _pytest.tmpdir.TempPathFactory) -> Path:
     """creates and maintains a session temp directory"""
     return tmp_path_factory.mktemp("tests_")
