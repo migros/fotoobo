afbeef5d95858d8e2f4bff3acf5f6d5ed8526dbb
Patrik Spiess <patrik.spiess@mgb.ch>
2023-04-06 13:16:33 +0200

    👔 🔧 Remove backup_dir from fotoobo.yaml closes #78

--- tests/helpers/test_config.py
+++ tests/helpers/test_config.py
@@ -16,40 +16,39 @@ class TestConfig:
     def test_config(self) -> None:
         """test the default config settings"""
         config = Config()
-        assert config.backup_dir == ""
         assert config.inventory_file == "inventory.yaml"
         assert not config.logging
         assert not config.audit_logging
         assert not config.no_logo
 
     @pytest.mark.parametrize(
-        "config_file,isfile_mock,load_yaml_file_mock,expected_backup_dir",
+        "config_file,isfile_mock,load_yaml_file_mock,expected_inventory",
         (
             pytest.param(
                 None,
                 MagicMock(return_value=False),
                 MagicMock(return_value=None),
-                "",
+                "inventory.yaml",
                 id="No config file given",
             ),
             pytest.param(
                 "test/fotoobo.yaml",
                 MagicMock(return_value=True),
-                MagicMock(return_value={"backup_dir": "test1", "logging": {"enabled": True}}),
+                MagicMock(return_value={"inventory": "test1", "logging": {"enabled": True}}),
                 "test1",
                 id="Custom fotoobo.yaml",
             ),
             pytest.param(
                 None,
                 lambda file: file == "fotoobo.yaml",
-                MagicMock(return_value={"backup_dir": "test2", "audit_logging": {"enabled": True}}),
+                MagicMock(return_value={"inventory": "test2", "audit_logging": {"enabled": True}}),
                 "test2",
                 id="Use fotoobo.yaml in current directory",
             ),
             pytest.param(
                 None,
                 lambda file: file != "fotoobo.yaml",
-                MagicMock(return_value={"backup_dir": "test3"}),
+                MagicMock(return_value={"inventory": "test3"}),
                 "test3",
                 id="Use fotoobo.yaml in .config/fotoobo.yaml",
             ),
@@ -61,7 +60,7 @@ class TestConfig:
         config_file: Union[str, None],
         isfile_mock: Union[MagicMock, Callable[[str], bool]],
         load_yaml_file_mock: MagicMock,
-        expected_backup_dir: str,
+        expected_inventory: str,
         monkeypatch: MonkeyPatch,
     ) -> None:
         """test load configuration from file"""
@@ -69,4 +68,4 @@ class TestConfig:
         monkeypatch.setattr("fotoobo.helpers.config.load_yaml_file", load_yaml_file_mock)
         config = Config()
         config.load_configuration(config_file)
-        assert config.backup_dir == expected_backup_dir
+        assert config.inventory_file == expected_inventory
