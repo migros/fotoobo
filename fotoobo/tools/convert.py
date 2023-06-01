ab477cff307ad11da6e6d0bfe87d7b035fb86cb0
Lukas Murer-Jäckle <lukas.murer@onway.ch>
2023-06-01 11:05:36 +0200

    :sparkles: Rewrite the convert checkpoint part to use the new Result class to return the values + fix a bug that caused the object put in to the converter be modified

--- fotoobo/tools/convert.py
+++ fotoobo/tools/convert.py
@@ -4,32 +4,42 @@ The fotoobo converter utility
 
 import logging
 from pathlib import Path
-from typing import Optional
+from typing import Any, Dict, List, Optional, Union
 
 from fotoobo.fortinet.convert import CheckpointConverter
-from fotoobo.helpers.files import create_dir, load_json_file, save_json_file
+from fotoobo.helpers.files import create_dir
+from fotoobo.helpers.result import Result
 
 log = logging.getLogger("fotoobo")
 
 
 def checkpoint(
-    infile: Path, outfile: Path, obj_type: str, cache_dir: Optional[Path] = None
-) -> None:
+    checkpoint_assets: Union[List[Any], Dict[str, Any], None],
+    obj_type: str,
+    conversion_id: str,
+    cache_dir: Optional[Path] = None,
+) -> Result:
     """
     This function lets you convert Checkpoint assets into Fortinet syntax.
 
     Args:
-        infile (Path): The exported Checkpoint assets file
-        outfile (Path): The file to write the converted Fortinet assets to
+        checkpoint_assets: The assets to convert
         obj_type (str): the type of asset to convert
+        conversion_id: An ID to the conversion. This is needed to cache different conversion tasks
         cache_dir (str, optional): The cache directory to use. Defaults to None.
     """
     log.info("start converting checkpoint assets of type %s", obj_type)
-    checkpoint_assets = load_json_file(infile)
+
+    result = Result()
+
     if cache_dir:
         create_dir(cache_dir)
+        cache_file = Path(cache_dir, conversion_id)
+    else:
+        cache_file = None
 
-    cache_file = Path(cache_dir, Path(outfile).name) if cache_dir else None
     converter = CheckpointConverter(checkpoint_assets, cache_file)
-    fortinet_assets = converter.convert(obj_type)
-    save_json_file(outfile, fortinet_assets)
+
+    result.push_result("fortinet_assets", converter.convert(obj_type))
+
+    return result
--- tests/cli/test_cli_convert.py
+++ tests/cli/test_cli_convert.py
@@ -1,13 +1,17 @@
 """
 Testing the cli convert app
 """
-
+import shutil
+from typing import Any, Dict
 from unittest.mock import MagicMock
+from pathlib import Path
 
+import pytest
 from _pytest.monkeypatch import MonkeyPatch
 from typer.testing import CliRunner
 
 from fotoobo.cli.main import app
+from fotoobo.helpers.files import load_json_file
 from tests.helper import parse_help_output
 
 runner = CliRunner()
@@ -34,9 +38,166 @@ def test_cli_convert_help() -> None:
 
 def test_cli_convert_checkpoint_unsupported(monkeypatch: MonkeyPatch) -> None:
     """Test convert cli command: convert checkpoint assets with unsupported type"""
-    monkeypatch.setattr("fotoobo.tools.convert.load_json_file", MagicMock(return_value=[]))
+    monkeypatch.setattr("fotoobo.cli.convert.load_json_file", MagicMock(return_value=[]))
     result = runner.invoke(
         app,
         ["-c", "tests/fotoobo.yaml", "convert", "checkpoint", "infile", "outfile", "unsupported"],
     )
     assert result.exit_code == 1
+
+
+@pytest.mark.parametrize(
+    "asset_type",
+    (
+        pytest.param("hosts", id="test type 'hosts'"),
+        pytest.param("networks", id="test type 'networks'"),
+        pytest.param("address_ranges", id="test type 'address_ranges'"),
+        pytest.param("groups", id="test type 'groups'"),
+        pytest.param("services_icmp", id="test type 'services_icmp'"),
+        pytest.param("services_icmp6", id="test type 'services_icmp6'"),
+        pytest.param("services_tcp", id="test type 'services_tcp'"),
+        pytest.param("services_udp", id="test type 'services_udp'"),
+        pytest.param("service_groups", id="test type 'service_groups'"),
+    ),
+)
+def test_cli_convert(asset_type: str, monkeypatch: MonkeyPatch, temp_dir: Path) -> None:
+    """Test convert"""
+    return_value: Dict[str, Any] = {
+        "hosts": [
+            {
+                "uid": "aaaa-aaaa-aaaa-aaaa",
+                "name": "dummy_host_1",
+                "ipv4-address": "1.1.1.1",
+                "comments": "dummy_comment_2",
+                "dummy": "dummy",
+            },
+            {
+                "uid": "bbbb-bbbb-bbbb-bbbb",
+                "name": "dummy_host_2",
+                "ipv4-address": "2.2.2.2",
+                "comments": "dummy_comment_2",
+                "dummy": "dummy",
+            },
+        ],
+        "networks": [
+            {
+                "name": "dummy_network_1",
+                "comments": "dummy_network_comment_1",
+                "subnet4": "1.1.1.0",
+                "mask-length4": 24,
+                "uid": "1111-1111-1111-1111",
+                "dummy": "dummy",
+            }
+        ],
+        "address_ranges": [],
+        "groups": [],
+        "services_icmp": [],
+        "services_icmp6": [],
+        "services_tcp": [],
+        "services_udp": [],
+        "service_groups": [],
+    }
+
+    monkeypatch.setattr("fotoobo.cli.convert.load_json_file", MagicMock(return_value=return_value))
+
+    output_file = temp_dir / f"convert_{asset_type}.json"
+    result = runner.invoke(
+        app,
+        [
+            "-c",
+            "tests/fotoobo.yaml",
+            "convert",
+            "checkpoint",
+            "testfile",
+            str(output_file),
+            asset_type,
+        ],
+    )
+
+    assert result.exit_code == 0
+    assert output_file.is_file()
+    converted = list(load_json_file(output_file) or [])
+
+    for index in range(len(return_value[asset_type])):
+        asset = converted[0]["params"][index]["data"]
+        assert asset["name"] == return_value[asset_type][index]["name"]
+        assert asset["comment"] == return_value[asset_type][index]["comments"]
+
+
+@pytest.mark.parametrize(
+    "asset_type",
+    (
+        pytest.param("hosts", id="test type 'hosts'"),
+        pytest.param("networks", id="test type 'networks'"),
+        pytest.param("address_ranges", id="test type 'address_ranges'"),
+        pytest.param("groups", id="test type 'groups'"),
+        pytest.param("services_icmp", id="test type 'services_icmp'"),
+        pytest.param("services_icmp6", id="test type 'services_icmp6'"),
+        pytest.param("services_tcp", id="test type 'services_tcp'"),
+        pytest.param("services_udp", id="test type 'services_udp'"),
+        pytest.param("service_groups", id="test type 'service_groups'"),
+    ),
+)
+def test_cli_convert_with_cache(asset_type: str, monkeypatch: MonkeyPatch, temp_dir: Path) -> None:
+    """Test convert with cache"""
+    return_value: Dict[str, Any] = {
+        "hosts": [
+            {
+                "uid": "aaaa-aaaa-aaaa-aaaa",
+                "name": "dummy_1",
+                "ipv4-address": "1.1.1.1",
+                "comments": "dummy_comment_1",
+                "dummy": "dummy",
+            },
+            {
+                "uid": "cccc-cccc-cccc-cccc",
+                "name": "dummy_3",
+                "ipv4-address": "3.3.3.3",
+                "comments": "dummy_comment_3",
+                "dummy": "dummy",
+            },
+        ],
+        "networks": [],
+        "address_ranges": [],
+        "groups": [],
+        "services_icmp": [],
+        "services_icmp6": [],
+        "services_tcp": [],
+        "services_udp": [],
+        "service_groups": [],
+    }
+
+    monkeypatch.setattr("fotoobo.cli.convert.load_json_file", MagicMock(return_value=return_value))
+
+    output_file = temp_dir / f"convert_cache_{asset_type}.json"
+    cache_dir = temp_dir / "cache"
+
+    if not cache_dir.is_dir():
+        cache_dir.mkdir()
+
+    shutil.copy(
+        Path("tests", "data", "convert_cache_hosts.json"),
+        cache_dir / "convert_cache_hosts.json",
+    )
+
+    result = runner.invoke(
+        app,
+        [
+            "-c",
+            "tests/fotoobo.yaml",
+            "convert",
+            "checkpoint",
+            "testfile",
+            str(output_file),
+            asset_type,
+            str(cache_dir),
+        ],
+    )
+
+    assert result.exit_code == 0
+    assert output_file.is_file()
+
+    converted = list(load_json_file(output_file) or [])
+    if asset_type == "hosts":
+        assert len(converted[0]["params"]) == 1
+        assert converted[0]["params"][0]["data"]["name"] == return_value["hosts"][1]["name"]
