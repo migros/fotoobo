ded2d89fb26c436ac913661dd806f82730c921cc
Lukas Murer-Jäckle <lukas.murer@onway.ch>
2023-06-23 16:58:42 +0200

    :sparkles: Remove parts of the Output class and output helpers that are now part of Result and add testing for Result

--- tests/helpers/test_output.py
+++ tests/helpers/test_output.py
@@ -3,23 +3,14 @@ Test the output helper
 """
 
 from pathlib import Path
-from typing import Any, List, Union
-from unittest.mock import MagicMock
+from typing import Any
 
 import pytest
-from _pytest.monkeypatch import MonkeyPatch
 
-from fotoobo.exceptions import GeneralWarning
-from fotoobo.helpers import cli_path
 from fotoobo.helpers.output import (
-    Output,
-    print_datatable,
-    print_dicttable,
-    print_json,
     print_logo,
     write_policy_to_html,
 )
-from fotoobo.inventory.generic import GenericDevice
 
 
 @pytest.fixture
@@ -28,210 +19,11 @@ def html_test_file(temp_dir: str) -> Path:
     return Path(temp_dir) / "test_file.html"
 
 
-class TestOutput:
-    """Test Output class"""
-
-    @staticmethod
-    def test_init() -> None:
-        """Test the output __init__"""
-        assert isinstance(Output().messages, list)
-
-    @staticmethod
-    @pytest.mark.parametrize(
-        "inputs, expect",
-        (
-            pytest.param("test", 1, id="add a string"),
-            pytest.param(["test_1", "test_2"], 2, id="add a list of strings"),
-        ),
-    )
-    def test_add(inputs: Union[str, List[str]], expect: int) -> None:
-        """Test the file_to_ftp function"""
-        output = Output()
-        output.add(inputs)
-        assert len(output.messages) == expect
-
-    @staticmethod
-    @pytest.mark.parametrize(
-        "inputs",
-        (
-            pytest.param({}, id="test with dict"),
-            pytest.param(1, id="test with int"),
-            pytest.param(1.5, id="test with float"),
-            pytest.param(True, id="test with bool"),
-            pytest.param(("1", "2"), id="test with tuple"),
-            pytest.param({"1", "2"}, id="test with set"),
-        ),
-    )
-    def test_add_exception(inputs: Any) -> None:
-        """Test add with unsupported type"""
-        with pytest.raises(GeneralWarning):
-            output = Output()
-            output.add(inputs)
-
-    @staticmethod
-    def test_print_raw(capsys: Any) -> None:
-        """Test print_raw"""
-        output = Output()
-        output.add(["dummy_line_1", "dummy_line_2"])
-        output.print_raw()
-        out, _ = capsys.readouterr()
-        assert "dummy_line_1" in out
-        assert "dummy_line_2" in out
-
-    @staticmethod
-    def test_send_mail(monkeypatch: MonkeyPatch) -> None:
-        """Test send_mail
-        Here we do not really test something. We just run through the send_mail method to see if
-        it raises any error or exception. In fact no mail is really sent.
-        """
-        monkeypatch.setattr(
-            "fotoobo.helpers.output.smtplib.SMTP.__init__", MagicMock(return_value=None)
-        )
-        monkeypatch.setattr(
-            "fotoobo.helpers.output.smtplib.SMTP.sendmail", MagicMock(return_value={})
-        )
-        output = Output()
-        output.send_mail(
-            GenericDevice(
-                hostname="dummy.local",
-                recipient="fotoobo_test@domain",
-                sender="fotoobo_test@domain",
-                subject="fotoobo test notification",
-            )
-        )
-        output.add(["dummy_line_1", "dummy_line_2"])
-        cli_path.append("dummy_cli_path")
-        output.send_mail(
-            GenericDevice(
-                hostname="dummy.local",
-                recipient="fotoobo_test@domain",
-                sender="fotoobo_test@domain",
-                subject="fotoobo test notification",
-            )
-        )
-        assert True
-
-
 def test_print_logo(capsys: Any) -> None:
     """Test print_logo"""
     print_logo()
     out, _ = capsys.readouterr()
     assert "f o t o o b o" in out
-
-
-@pytest.mark.parametrize(
-    "input_data",
-    (
-        pytest.param({"key1": "val1", "key2": "val2"}, id="test with dict"),
-        pytest.param([{"key1": "val1", "key2": "val2"}], id="test with list"),
-    ),
-)
-def test_print_datatable(input_data: Any, capsys: Any) -> None:
-    """Test print_datatable"""
-    print_datatable(input_data)
-    out, _ = capsys.readouterr()
-    assert "key1" not in out
-    assert "val1" in out
-    assert "key2" not in out
-    assert "val2" in out
-
-
-def test_print_datatable_with_dict(capsys: Any) -> None:
-    """Test print_datatable when a dict is given"""
-    print_datatable({"key1": "val1", "key2": "val2"})
-    out, _ = capsys.readouterr()
-    assert "key1" not in out
-    assert "val1" in out
-    assert "key2" not in out
-    assert "val2" in out
-
-
-def test_print_datatable_auto_header(capsys: Any) -> None:
-    """Test print_datatable with auto_header"""
-    print_datatable([{"key1": "val1", "key2": "val2"}], auto_header=True)
-    out, _ = capsys.readouterr()
-    assert "key1" in out
-    assert "val1" in out
-
-
-def test_print_datatable_with_headers(capsys: Any) -> None:
-    """Test print_datatable with headers given"""
-    print_datatable([{"key1": "val1", "key2": "val2"}], headers=["keys", "values"])
-    out, _ = capsys.readouterr()
-    assert "keys" in out
-    assert "key1" not in out
-    assert "val1" in out
-
-
-def test_print_datatable_with_title(capsys: Any) -> None:
-    """Test print_datatable with title given"""
-    print_datatable([{"key1": "val1", "key2": "val2"}], title="title")
-    out, _ = capsys.readouterr()
-    assert "title" in out
-
-
-@pytest.mark.parametrize(
-    "input_data",
-    (
-        pytest.param(("1", "2"), id="test with tuple"),
-        pytest.param({"1", "2"}, id="test with set"),
-        pytest.param("dummy", id="test with str"),
-        pytest.param(42, id="test with int"),
-        pytest.param(4.2, id="test with float"),
-        pytest.param(True, id="test with bool"),
-        pytest.param(None, id="test with NoneType"),
-    ),
-)
-def test_print_datatable_unsupported_type(input_data: Any) -> None:
-    """Test print_datatable when data given is unsupported"""
-    with pytest.raises(GeneralWarning, match=r"data is not a list or dict"):
-        print_datatable(input_data)
-
-
-def test_print_dicttable(capsys: Any) -> None:
-    """Test print_dicttable"""
-    print_dicttable({"key1": "val1", "key2": "val2"})
-    out, _ = capsys.readouterr()
-    assert "key1" in out
-    assert "val1" in out
-    assert "key2" in out
-    assert "val2" in out
-
-
-@pytest.mark.parametrize(
-    "input_data",
-    (
-        pytest.param(["1", "2"], id="test with list"),
-        pytest.param(("1", "2"), id="test with tuple"),
-        pytest.param({"1", "2"}, id="test with set"),
-        pytest.param("dummy", id="test with str"),
-        pytest.param(42, id="test with int"),
-        pytest.param(4.2, id="test with float"),
-        pytest.param(True, id="test with bool"),
-        pytest.param(None, id="test with NoneType"),
-    ),
-)
-def test_print_dicttable_unsupported_type(input_data: Any) -> None:
-    """Test print_dicttable when data given is unsupported"""
-    with pytest.raises(GeneralWarning, match=r"data is not a dict"):
-        print_dicttable(input_data)
-
-
-@pytest.mark.parametrize(
-    "input_data, expect",
-    (
-        pytest.param("dummy", "dummy", id="test with str"),
-        pytest.param(["dummy"], "dummy", id="test with list"),
-        pytest.param({"dummy": 42}, "dummy", id="test with dict"),
-        pytest.param(42, "42", id="test with int"),
-        pytest.param(4.2, "4.2", id="test with float"),
-    ),
-)
-def test_print_json(input_data: Any, expect: str, capsys: Any) -> None:
-    """Test print_json"""
-    print_json(input_data)
-    out, _ = capsys.readouterr()
-    assert expect in out
 
 
 def test_write_policy_to_html(html_test_file: Path) -> None:  # pylint: disable=redefined-outer-name
--- tests/helpers/test_results.py
+++ tests/helpers/test_results.py
@@ -4,15 +4,264 @@ Test the results helper class
 
 
 from pathlib import Path
-from typing import Dict
+from typing import Any, Dict, List, Tuple, Union
+from unittest.mock import MagicMock
 
+import pytest
+from _pytest.monkeypatch import MonkeyPatch
+
+from fotoobo.exceptions import GeneralWarning
+from fotoobo.helpers import cli_path
 from fotoobo.helpers.result import Result
+from fotoobo.inventory import GenericDevice
 
 
 class TestResults:
     """Test the results class"""
 
-    def test_save_with_template(self, temp_dir: Path) -> None:
+    @staticmethod
+    def test_init() -> None:
+        """Test the result __init__"""
+        result = Result[str]()
+        assert isinstance(result.messages, dict)
+        assert isinstance(result.results, dict)
+
+    @staticmethod
+    @pytest.mark.parametrize(
+        "inputs,successful,expected_result",
+        (
+            pytest.param("test", None, {"test_host": "test"}, id="add a string"),
+            pytest.param(
+                ["test_1", "test_2"],
+                True,
+                {"test_host": ["test_1", "test_2"]},
+                id="add a list of strings",
+            ),
+            pytest.param("test", False, {"test_host": "test"}, id="unsuccessful"),
+        ),
+    )
+    def test_push_result(
+        inputs: Union[str, List[str]],
+        successful: Union[bool, None],
+        expected_result: Dict[str, Any],
+    ) -> None:
+        """Test the push_result() method"""
+        result = Result[Any]()
+        if successful is None:
+            result.push_result("test_host", inputs)
+        else:
+            result.push_result("test_host", inputs, successful=successful)
+
+        assert result.results == expected_result
+
+        if successful in [None, True]:
+            assert "test_host" in result.successful
+            assert len(result.successful) == 1
+            assert len(result.failed) == 0
+
+        else:
+            assert "test_host" in result.failed
+            assert len(result.successful) == 0
+            assert len(result.failed) == 1
+
+    @staticmethod
+    @pytest.mark.parametrize(
+        "message,level",
+        (
+            pytest.param("test message", None, id="default level"),
+            pytest.param("test message", "blabla", id="some custom level"),
+        ),
+    )
+    def test_push_message(message: str, level: Union[None, str]) -> None:
+        """Test the push_message() method"""
+
+        result = Result[Any]()
+        if level is None:
+            result.push_message("test_host", message)
+        else:
+            result.push_message("test_host", message, level)
+
+        assert "test_host" in result.messages
+        assert isinstance(result.messages["test_host"], list)
+        assert result.messages["test_host"][0] == {"message": message, "level": level or "info"}
+
+    @staticmethod
+    def test_get_messages() -> None:
+        """Test the get_messages()  method"""
+
+        result = Result[Any]()
+        result.push_message("test_host", "test message 1")
+        result.push_message("test_host", "test message 2", level="warning")
+
+        # Check the messages got from get_messages()
+        messages = result.get_messages("test_host")
+
+        assert len(messages) == 2
+        assert {"message": "test message 1", "level": "info"} in messages
+        assert {"message": "test message 2", "level": "warning"} in messages
+
+        # Non-existing host should return empty list
+        assert len(result.get_messages("nonexisting_host")) == 0
+
+    @staticmethod
+    @pytest.mark.parametrize(
+        "messages,only_host,expected_outputs",
+        (
+            pytest.param(
+                [("test_host", "dummy_line_1"), ("test_host", "dummy_line_2")],
+                None,
+                ["dummy_line_1", "dummy_line_2"],
+                id="default messages",
+            ),
+            pytest.param(
+                [("test_host", "dummy_line_1"), ("test_host", "dummy_line_2", "warning")],
+                None,
+                ["dummy_line_1", "dummy_line_2"],
+                id="message with level 'warning",
+            ),
+            pytest.param(
+                [("test_host", "dummy_line_1"), ("test_host1", "dummy_line_2", "warning")],
+                "test_host",
+                ["dummy_line_1"],
+                id="print only messages for one host",
+            ),
+        ),
+    )
+    def test_print_messages(
+        messages: List[Tuple[str]],
+        only_host: Union[str, None],
+        expected_outputs: List[str],
+        capsys: Any,
+    ) -> None:
+        """Test print_messages() method"""
+        result = Result[Any]()
+        for message in messages:
+            result.push_message(*message)  # type: ignore
+
+        result.print_messages(only_host)
+        out, _ = capsys.readouterr()
+
+        for out_message in expected_outputs:
+            assert out_message in out
+
+    @staticmethod
+    def test_get_result() -> None:
+        """Test the get_result() method"""
+        result = Result[str]()
+        result.push_result("test_host", "test_result")
+
+        assert result.get_result("test_host") == "test_result"
+        with pytest.raises(GeneralWarning, match=r"Host nonexisting_host is not in results."):
+            result.get_result("nonexisting_host")
+
+    @staticmethod
+    def test_all_results() -> None:
+        """Test the all_result() method"""
+        result = Result[str]()
+        result.push_result("test_host", "test_result")
+
+        assert result.all_results() == {"test_host": "test_result"}
+
+    @staticmethod
+    @pytest.mark.parametrize(
+        "input_data",
+        (
+            pytest.param({"key1": "val1", "key2": "val2"}, id="test with dict"),
+            pytest.param([{"key1": "val1", "key2": "val2"}], id="test with list"),
+        ),
+    )
+    def test_print_result_as_table(input_data: Any, capsys: Any) -> None:
+        """Test print_result_as_table() with default values for one host"""
+        result = Result[Any]()
+        result.push_result("test_host", input_data)
+
+        result.print_result_as_table("test_host")
+
+        out, _ = capsys.readouterr()
+        assert "key1" not in out
+        assert "val1" in out
+        assert "key2" not in out
+        assert "val2" in out
+
+    @staticmethod
+    def test_print_result_as_table_auto_header(capsys: Any) -> None:
+        """Test print_result_as_table() method with auto_header=True"""
+        result = Result[Dict[str, str]]()
+        result.push_result("test_host", {"key1": "val1", "key2": "val2"})
+
+        result.print_result_as_table("test_host", auto_header=True)
+
+        out, _ = capsys.readouterr()
+        assert "key1" in out
+        assert "val1" in out
+
+    @staticmethod
+    def test_print_result_as_table_with_headers(capsys: Any) -> None:
+        """Test print_result_as_table() method with headers given"""
+        result = Result[Dict[str, str]]()
+        result.push_result("test_host", {"key1": "val1", "key2": "val2"})
+
+        result.print_result_as_table("test_host", headers=["keys", "values"])
+
+        out, _ = capsys.readouterr()
+        assert "keys" in out
+        assert "key1" not in out
+        assert "val1" in out
+
+    @staticmethod
+    def test_print_result_as_table_with_title(capsys: Any) -> None:
+        """Test print_result_as_table() method with title given"""
+        result = Result[Dict[str, str]]()
+        result.push_result("test_host", {"key1": "val1", "key2": "val2"})
+
+        result.print_result_as_table("test_host", title="title")
+
+        out, _ = capsys.readouterr()
+        assert "title" in out
+
+    @staticmethod
+    @pytest.mark.parametrize(
+        "input_data",
+        (
+            pytest.param(("1", "2"), id="test with tuple"),
+            pytest.param({"1", "2"}, id="test with set"),
+            pytest.param("dummy", id="test with str"),
+            pytest.param(42, id="test with int"),
+            pytest.param(4.2, id="test with float"),
+            pytest.param(True, id="test with bool"),
+            pytest.param(None, id="test with NoneType"),
+        ),
+    )
+    def test_print_table_raw_with_unsupported_data(input_data: Any) -> None:
+        """Test print_table_raw() when data given is unsupported"""
+        result = Result[Any]()
+
+        with pytest.raises(GeneralWarning, match=r"must be a list of dicts"):
+            result.print_table_raw(input_data, [])
+
+    @staticmethod
+    @pytest.mark.parametrize(
+        "input_data, expect",
+        (
+            pytest.param("dummy", "dummy", id="test with str"),
+            pytest.param(["dummy"], "dummy", id="test with list"),
+            pytest.param({"dummy": 42}, "dummy", id="test with dict"),
+            pytest.param(42, "42", id="test with int"),
+            pytest.param(4.2, "4.2", id="test with float"),
+        ),
+    )
+    def test_print_raw(input_data: Any, expect: str, capsys: Any) -> None:
+        """Test print_raw() method"""
+        result = Result[Any]()
+        result.push_result("test_host", input_data)
+
+        result.print_raw("test_host")
+
+        out, _ = capsys.readouterr()
+        assert expect in out
+
+    @staticmethod
+    def test_save_with_template(temp_dir: Path) -> None:
         """Test save_with_template"""
         result = Result[Dict[str, Dict[str, int]]]()
         result.push_result("dummy_ems", {"fotoobo": {"dummy_var": 42}})
@@ -22,3 +271,46 @@ class TestResults:
         content = output_file.read_text(encoding="UTF-8")
         assert "dummy" in content
         assert "42" in content
+
+    @staticmethod
+    def test_send_messages_as_mail(monkeypatch: MonkeyPatch) -> None:
+        """Test send_messages_as_mail
+        Here we do not really test something. We just run through the send_mail method to see if
+        it raises any error or exception. In fact no mail is really sent.
+        """
+        sendmail_mock = MagicMock()
+
+        monkeypatch.setattr(
+            "fotoobo.helpers.result.smtplib.SMTP.__init__", MagicMock(return_value=None)
+        )
+        monkeypatch.setattr(
+            "fotoobo.helpers.result.smtplib.SMTP.__enter__", MagicMock(return_value=sendmail_mock)
+        )
+        cli_path.append("dummy_cli_path")
+
+        # Prepare result
+        result = Result[Any]()
+        result.push_message("test_host", "dummy line 1")
+        result.push_message("test_host", "dummy line 2")
+
+        result.send_messages_as_mail(
+            GenericDevice(
+                hostname="dummy.local",
+                recipient="fotoobo_recipient@domain",
+                sender="fotoobo_sender@domain",
+                subject="fotoobo test notification",
+            )
+        )
+
+        mail = (
+            "To:fotoobo_recipient@domain\n"
+            "From:fotoobo_sender@domain\n"
+            "Subject:fotoobo test notification\n"
+            "\n"
+            "test_host: dummy line 1\n"
+            "test_host: dummy line 2\n"
+        )
+
+        sendmail_mock.sendmail.assert_called_with(
+            "fotoobo_sender@domain", "fotoobo_recipient@domain", mail
+        )
