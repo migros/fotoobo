17601cd32d765255a13317a6de20865f2b6481fc
Patrik Spiess <patrik.spiess@mgb.ch>
2023-08-03 08:10:26 +0200

    ⚡️ 🎨 Improve FMG/FAZ session handling closes #94

--- fotoobo/tools/fmg/get.py
+++ fotoobo/tools/fmg/get.py
@@ -151,9 +151,8 @@ def version(host: str) -> Result[str]:
     inventory = Inventory(config.inventory_file)
     fmg = inventory.get(host, "fortimanager")[host]
     log.debug("FortiManager get version ...")
-    fmg.login()
+    # fmg.login()
     fmg_version = fmg.get_version()
-    fmg.logout()
     result = Result[str]()
     result.push_result(host, fmg_version)
 
