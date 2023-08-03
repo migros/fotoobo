55e3a0bf63806e25fc4a294c0d30957b26a62ab7
Patrik Spiess <patrik.spiess@mgb.ch>
2023-08-03 13:01:48 +0200

    👷  Improve release process WHATSNEW.md now handled correctly

--- .tag_release.sh
+++ .tag_release.sh
@@ -9,4 +9,14 @@ git tag -a "v$new_version" -m "Version v$new_version"
 git push origin "v$new_version"
 
 echo "Done. Version $new_version is now released on GitHub (and soon on PyPi, etc.)."
-echo -e "Congratulations \U2727"
+echo -e "Congratulations"
+
+
+# After the release we have to empty the WHATSNEW file and commit/push it again
+echo "Emptiying WHATSNEW.md"
+echo "### Added\n\n" > WHATSNEW.md
+echo "### Changed\n\n" >> WHATSNEW.md
+echo "### Removed\n\n" >> WHATSNEW.md
+git add WHATSNEW.md
+git commit -m ":memo: Reset WHATSNEW.md with template"
+git push
