ec57670910b67e1e8f940d6c405125923fe2e7e2
Patrik Spiess <patrik.spiess@mgb.ch>
2023-11-27 09:52:42 +0100

    🔐 Read vault secrets from environment closes #169

--- tests/helpers/test_config.py
+++ tests/helpers/test_config.py
@@ -1,20 +1,23 @@
 """
 Test the config helper
 """
-from typing import Callable, Union
+import os
 from pathlib import Path
+from typing import Callable, Union
 from unittest.mock import MagicMock
 
 import pytest
 from _pytest.monkeypatch import MonkeyPatch
 
+from fotoobo.exceptions.exceptions import GeneralError
 from fotoobo.helpers.config import Config
 
 
 class TestConfig:
     """Test the config dataclass"""
 
-    def test_config(self) -> None:
+    @staticmethod
+    def test_config() -> None:
         """test the default config settings"""
         config = Config()
         assert config.inventory_file == Path("inventory.yaml")
@@ -22,6 +25,7 @@ class TestConfig:
         assert not config.audit_logging
         assert not config.no_logo
 
+    @staticmethod
     @pytest.mark.parametrize(
         "config_file,isfile_mock,load_yaml_file_mock,expected_inventory",
         (
@@ -55,9 +59,7 @@ class TestConfig:
             ),
         ),
     )
-    # pylint: disable=too-many-arguments
     def test_load_configuration(
-        self,
         config_file: Union[Path, None],
         isfile_mock: Union[MagicMock, Callable[[str], bool]],
         load_yaml_file_mock: MagicMock,
@@ -70,3 +72,46 @@ class TestConfig:
         config = Config()
         config.load_configuration(config_file)
         assert config.inventory_file == expected_inventory
+
+    @staticmethod
+    @pytest.mark.parametrize(
+        "env,yaml,expected",
+        (
+            pytest.param(True, True, "from_env", id="env and yaml set"),
+            pytest.param(True, False, "from_env", id="only env set"),
+            pytest.param(False, True, "from_yaml", id="only yaml set"),
+            pytest.param(False, False, "from_yaml", id="env and yaml not set"),
+        ),
+    )
+    def test_config_vault(env: bool, yaml: bool, expected: str, monkeypatch: MonkeyPatch) -> None:
+        """Test the vault part of the configuration"""
+        test_config = Config()
+        vault_config = {
+            "url": "https://vault.local",
+            "namespace": "vault_namespace",
+            "data_path": "/v1/kv/data/fotoobo",
+        }
+        if yaml:
+            vault_config["role_id"] = "role_id_from_yaml"
+            vault_config["secret_id"] = "secret_id_from_yaml"
+
+        monkeypatch.setattr(
+            "fotoobo.helpers.config.load_yaml_file", MagicMock(return_value={"vault": vault_config})
+        )
+
+        if env:
+            os.environ["FOTOOBO_VAULT_ROLE_ID"] = "role_id_from_env"
+            os.environ["FOTOOBO_VAULT_SECRET_ID"] = "secret_id_from_env"
+
+        else:
+            os.environ.pop("FOTOOBO_VAULT_ROLE_ID", True)
+            os.environ.pop("FOTOOBO_VAULT_SECRET_ID", True)
+
+        if env or yaml:
+            test_config.load_configuration(Path("tests/fotoobo.yaml"))
+            assert test_config.vault["url"] == "https://vault.local"
+            assert test_config.vault["role_id"].endswith(expected)
+
+        else:
+            with pytest.raises(GeneralError, match=r"Missing vault configuration data:.*"):
+                test_config.load_configuration(Path("tests/fotoobo.yaml"))
